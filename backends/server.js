db2a4a80dbfacf27f72a8e82bc4fc2f9950bd6c6
codevil <codevil0209@gmail.com>
2025-09-20 20:17:55 +0530

    added cors

--- backends/server.js
+++ backends/server.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
